411e3e091a8c359251ed0bd42bb92ccaba4eee13
rkuczer <ryan-kooz@hotmail.com>
2020-03-03 14:20:08 -0500

    second committ

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,11 @@
-public class Employee {
+public abstract class Employee {
+    abstract public double calculateWeeklyPay();
+
+    public void withholdTaxes(){
+        System.out.println("We are witholding " + calculateWeeklyPay()*.1 + " from your paycheck");
+    }
+
+    public void fileW2Forms(){
+        System.out.print("One copy for you and one for the IRS.");
+    }
 }
